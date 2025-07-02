944022a34ff752a6d06ad2d37e5618e294693a77
jrdevmax <jr.anegocios@gmail.com>
2025-07-02 11:49:06 -0300

    Create loops.java

---
+++ secaofor/loops.java
@@ -0,0 +1,60 @@
+package secaofor;
+
+import java.util.Scanner;
+
+public class loops {
+   
+   public static void main(String[] args){
+      Scanner scan = new Scanner(System.in);
+
+     
+      scan.close();
+      // String palavra = "Java";
+      // for(int i=0; i < palavra.length(); i++){
+      //    System.out.println("contador "+ palavra.charAt(i));
+      // }
+
+      // 2 - while
+      // int i = 0;
+      // while(i<=5){
+      //    System.out.println("contador while"+ i);
+      //    i++;
+      // }
+      // 5- break
+      // for(int x=0; x <=10; x++){
+      //    System.out.println("O valor de x é "+x);
+      //    if(x == 5){
+      //       System.out.println("parando loop");
+      //       break;
+      //    }
+      // }
+      // 6 - continui
+      // for(int i=0; i<8; i++){
+      //    System.out.println("contador "+i);
+      //    if(i % 2 ==0){
+      //       // System.out.println("continuando o loop");
+      //       continue;
+      //    }
+      // }
+
+      // nested loop
+      // for(int i=1; i <=3; i++){
+      //    System.out.println("EXTERNO");
+      //    for(int n = 1; n<=3; n++){
+      //       System.out.println(i + "x" + n +" =" + (i * n));
+      //    }
+      // }
+
+      /*
+       * criar um programa que calcule o somatorio dos numero de 1
+       * a 100 e exibir o resultado
+      //  */
+      // int somatorio =0 ;
+      // for(int i=0; i<=100;i++){
+      //    somatorio +=i;
+      // }
+      // System.out.println("A soma de 1 ate 100 : "+somatorio);
+      
+   }  
+
+}
