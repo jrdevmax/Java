ec04b1508a244272f7cf8b87eedb6da5bfba32c3
jrdevmax <jr.anegocios@gmail.com>
2025-07-02 11:14:50 -0300

    Atualizar o Calculadora.java
    construir switch pra validar as opcoes

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 public class Calculadora{
-   public static void main(String[] args){
+   public static void main(String[] args)     {
+    // estâncias objeto scan do tipo Scanner
+    Scanner Scanner = New Scanner(System.in);
+    System.out.println(" 1) Multiplicar");
+    System.out.println(" 2) Dividir");
+    System.out.println(" 3) Subtrair");
 
+    int opa = Scanner.nextInt();
+    
+    // switch verifica e válida se a opção digitada confere como a declaração 
+    switch(op){
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        default:
+
+    }
 }
 
 }
