59993f5d1471ae4c35086bc4f13433f426d9b85f
jrdevmax <jr.anegocios@gmail.com>
2025-07-04 20:25:30 -0300

    criei as funcoes dividir e subitrair

--- calculador/Calculadora.java
+++ calculador/Calculadora.java
@@ -17,11 +17,15 @@ import java.util.Scanner;
             switch(op){
                 case 1:
                     System.out.println("======= MULTIPLICAR SELECIONADA ======="); 
-                    multiplicar();                  
+                    multiplicar();          
                 break;
                 case 2:
+                  System.out.println("======= DIVIDIR SELECIONADA ======="); 
+                    dividir();     
                 break;
                 case 3:
+                    System.out.println("======= SUBTRAIR SELECIONADA ======="); 
+                    subtrair();    
                 break;
                 default:
 
@@ -32,21 +36,40 @@ import java.util.Scanner;
 
         public static void multiplicar(){
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Entre com dois numero que  deseja multiplicar");
+            System.out.println("Type first number");
+           
             double a = scanner.nextDouble();
+             System.out.println("Type the second number");
             double b = scanner.nextDouble();
-
             scanner.close();
-            System.out.println("deseja multiplicar outro numero ? (s/n)");
             
-            int opcao = scanner.nextInt();
-            if(opcao == 's'){
-                multiplicar();
-            }
           double result = (a * b);
-          System.out.println(result);
-          
-          
+          System.out.println("Result : "+result);
         }
 
+        public static void dividir(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Type first number");
+           
+            double a = scanner.nextDouble();
+             System.out.println("Type the second number");
+            double b = scanner.nextDouble();
+            scanner.close();
+            
+          double result = (a / b);
+          System.out.println("Result : "+result);
+        }
+
+         public static void subtrair(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Type first number");
+           
+            double a = scanner.nextDouble();
+             System.out.println("Type the second number");
+            double b = scanner.nextDouble();
+            scanner.close();
+            
+          double result = (a - b);
+          System.out.println("Result : "+result);
+        }
     }
